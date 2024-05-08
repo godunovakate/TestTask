3d4c0746285f898d5ac2b0b7639a3f190ed965f6
godunovakate <godunovakate12@gmail.com>
2024-05-08 08:57:46 +0300

    Arrays

---
+++ practice/Arrays/upOnTenPercent.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function tenPercent(array) {
+    return array.map(item => {
+        return item + item / 10;
+    });
+}
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(tenPercent(array));
