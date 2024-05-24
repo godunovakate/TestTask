353fd0915f63b42c097aeb3dcc52159ef4a25ae1
godunovakate <godunovakate12@gmail.com>
2024-05-24 23:44:14 +0300

    delete const api and add Environment Variables

--- src/constants/api.tsx
+++
@@ -1,2 +0,0 @@
-export const GOOGLE_BOOKS_API_URL = 'https://www.googleapis.com/books/v1/volumes';
-export const GOOGLE_BOOKS_API_KEY = 'AIzaSyCASKsQWlSSU553100lX-MQLBrFB3E7RLM';
--- src/utils/searchBook.tsx
+++ src/utils/searchBook.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 import { Book } from 'constants/bookInterface';
-import { GOOGLE_BOOKS_API_KEY, GOOGLE_BOOKS_API_URL } from 'constants/api';
 import mapData from './mapBookData';
 
-// Функция searchBook теперь принимает строку поиска в качестве аргумента
 const searchBook = (
   search: string,
   setSearch: React.Dispatch<React.SetStateAction<string>>,
   setData: React.Dispatch<React.SetStateAction<Book[]>>,
 ) => {
   axios
-    .get(`${GOOGLE_BOOKS_API_URL}?q=${search}&key=${GOOGLE_BOOKS_API_KEY}&maxResults=40`)
+    .get(`${process.env.REACT_APP_GOOGLE_BOOKS_API_URL}?q=${search}&key=${process.env.REACT_APP_GOOGLE_BOOKS_API_KEY}&maxResults=40`)
     .then(({ data: { items } }) => {
       const mappedData = items.map(mapData);
       setData(mappedData);
